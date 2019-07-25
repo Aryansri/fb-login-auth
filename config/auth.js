7422ca0551d780a3aaf068f163c888f0c7c7fcde
Aryansri <aryansri910@gmail.com>
2019-07-25 14:58:43 +0530

    fb login

---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '652876465193094',
+		'clientSecret': '8e79a3470585218f607e08e2aabe085c',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback',
+	},
+
+	// 'googleAuth' : {
+	// 	'clientID': 'enter client id here',
+	// 	'clientSecret': 'enter client secret here',
+	// 	'callbackURL': 'enter callback here'
+	// }
+}
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url'	: 'mongodb://Sudeep36:sant0551@ds149616.mlab.com:49616/rummy'
+}
